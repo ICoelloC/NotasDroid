547ea5c05c196df20c22e425cf9e01f86b5243f2
Ivan <ivancoellocortes@gmail.com>
2020-11-11 18:25:15 +0100

    Commit on dev branch, expediente's fragment has been designed.

--- app/src/main/java/com/icoelloc/notasdroid/ui/expediente/ExpedienteFragment.kt
+++ app/src/main/java/com/icoelloc/notasdroid/ui/expediente/ExpedienteFragment.kt
@@ -1,4 +1,4 @@
-package com.icoelloc.notasdroid.ui.home
+package com.icoelloc.notasdroid.ui.expediente
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -10,20 +10,20 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.icoelloc.notasdroid.R
 
-class HomeFragment : Fragment() {
+class ExpedienteFragment : Fragment() {
 
-    private lateinit var homeViewModel: HomeViewModel
+    private lateinit var expedienteViewModel: ExpedienteViewModel
 
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        homeViewModel =
-                ViewModelProvider(this).get(HomeViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_home, container, false)
+        expedienteViewModel =
+                ViewModelProvider(this).get(ExpedienteViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_expediente, container, false)
         val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
+        expedienteViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
         return root
--- app/src/main/java/com/icoelloc/notasdroid/ui/expediente/ExpedienteViewModel.kt
+++ app/src/main/java/com/icoelloc/notasdroid/ui/expediente/ExpedienteViewModel.kt
@@ -1,13 +1,13 @@
-package com.icoelloc.notasdroid.ui.home
+package com.icoelloc.notasdroid.ui.expediente
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class HomeViewModel : ViewModel() {
+class ExpedienteViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+        value = "MI EXPEDIENTE"
     }
     val text: LiveData<String> = _text
 }
